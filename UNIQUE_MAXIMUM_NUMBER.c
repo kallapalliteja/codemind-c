6964336a143d61b9714f178f8b0b9c879acf6e7b
kallapalliteja <96564047+kallapalliteja@users.noreply.github.com>
2022-02-24 22:23:12 +0530

    #include<stdio.h> int main() {     int n,i,j,c,max=0,k=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)       scanf("%d",&a[i]);
        for(i=0;i<n;i++)
          {       c=1;
               // k=0;
               
                 for(j=0;j<n;j++)
                {
    			 if(a[i]==a[j] && i!=j)
                   c++;
                }
               
          if(c==1)
          {
          	k++;
          	if(a[i]>max)
          	{
          		max=a[i];
    		  }
    	  }
        }
        if(k>0) printf("%d",max);
      else printf("-1");
    // printf("%d",k);
        
    }

---
+++ UNIQUE_MAXIMUM_NUMBER.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,c,max=0,k=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+      scanf("%d",&a[i]);
+      
+    for(i=0;i<n;i++)
+      {       c=1;
+           // k=0;
+           
+             for(j=0;j<n;j++)
+            {
+			 if(a[i]==a[j] && i!=j)
+               c++;
+            }
+           
+      if(c==1)
+      {
+      	k++;
+      	if(a[i]>max)
+      	{
+      		max=a[i];
+		  }
+	  }
+    }
+    if(k>0) printf("%d",max);
+  else printf("-1");
+// printf("%d",k);
+    
+}
