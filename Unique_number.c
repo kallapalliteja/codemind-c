4e90cc3afb0bc276b868a9c2d394b7f55a2925ef
kallapalliteja <96564047+kallapalliteja@users.noreply.github.com>
2022-04-29 12:05:57 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,r,i=0,k,j,c=0;     scanf("%d",&n);     k=log10(n)+1;     int a[k];     while(n)     {         r=n%10;         n=n/10;         a[i]=r;         i++;
        }
        for(i=0;i<k;i++)
        {
            for(j=0;j<k;j++)
            {
                if(a[i]==a[j] && i!=j)
                {
                    c++;
                }
            }
            
        }
        if(c==0) printf("Unique Number");
        else printf("Not Unique Number");
    }

---
+++ Unique_number.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,r,i=0,k,j,c=0;
+    scanf("%d",&n);
+    k=log10(n)+1;
+    int a[k];
+    while(n)
+    {
+        r=n%10;
+        n=n/10;
+        a[i]=r;
+        i++;
+        
+        
+    }
+    for(i=0;i<k;i++)
+    {
+        for(j=0;j<k;j++)
+        {
+            if(a[i]==a[j] && i!=j)
+            {
+                c++;
+            }
+        }
+        
+    }
+    if(c==0) printf("Unique Number");
+    else printf("Not Unique Number");
+}
